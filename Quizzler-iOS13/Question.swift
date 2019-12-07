ec760127743946908980406a8c6b57308f600251
balazs-varga <balazsvarga91@gmail.com>
2019-12-07 10:37:49 +0100

    create 3 Questions and loop throught them

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(text: String, answer: String) {
+        self.text = text
+        self.answer = answer
+    }
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -7,13 +7,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        Question(text: "Four + Two is equal to Six.", answer: "True"),
+        Question(text: "Five - Three is greater than One.", answer: "True"),
+        Question(text: "Three + Eight is less than Ten.", answer: "False"),
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if userAnswer == actualAnswer {
+            print("right")
+        } else {
+            print("wrong")
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
     }
     
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber].text
+    }
 }
 
